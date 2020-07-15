fe229130436a8e460a0758a60a05eee754c82339
git-leon <xleonhunter@gmail.com>
2020-07-15 16:56:17 -0400

    first commit

---
+++ src/main/java/com/github/curriculeon/Person.java
@@ -0,0 +1,32 @@
+package com.github.curriculeon;
+
+/**
+ * Created by leon on 2/12/18.
+ */
+public class Person {
+    public Person() {
+    }
+
+    public Person(int age) {
+    }
+
+    public Person(String name) {
+    }
+
+    public Person(String name, int age) {
+    }
+
+    public void setName(String name) {
+    }
+
+    public void setAge(int age) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public Integer getAge() {
+        return null;
+    }
+}
